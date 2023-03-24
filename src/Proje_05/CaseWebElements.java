7e823b2af09b8cd4b1904bf9d2675338b4adbc0a
MustafaKcdm <mmustafakocaadam@gmail.com>
2023-03-24 23:42:52 +0300

    c3

--- src/Proje_05/CaseWebElements.java
+++ src/Proje_05/CaseWebElements.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class CaseWebElements {
     public CaseWebElements() {
         PageFactory.initElements(BaseDriverParameter.driver,this);
@@ -20,6 +22,67 @@ public class CaseWebElements {
     public WebElement loginButton;
     @FindBy(xpath = "//h1[normalize-space()='Dashboard']")
     public WebElement Validation;
+
+    @FindBy(xpath = "//*[@class='nav-item has-treeview']")
+    public List<WebElement> leftNawMenu;
+
+    @FindBy(xpath = "//[@class='nav-icon far fa-user']")
+    public List<WebElement> customers;
+
+    @FindBy(css = "i[class='nav-icon far fa-user']")
+    public WebElement customersOne;
+
+    @FindBy(xpath = "//*[text()=' Customers']")
+    public WebElement customersTwo;
+    @FindBy(css ="a[class='btn btn-primary']")
+    public WebElement addNew;
+    @FindBy(id="Email")
+    public WebElement emailCustomer;
+    @FindBy(id="Password")
+    public WebElement passwordCustomer;
+    @FindBy(id="FirstName")
+    public WebElement firstName;
+    @FindBy(id="LastName")
+    public WebElement lastNamecustmr;
+    @FindBy(id="DateOfBirth")
+    public WebElement dateOBirth;
+    @FindBy(name="save")
+    public WebElement save;
+    @FindBy(css ="[class='alert alert-success alert-dismissable']")
+    public WebElement validationCustomer;
+
+    @FindBy(id="SearchFirstName")
+    public WebElement firstNamesearch;
+
+    @FindBy(id="SearchLastName")
+    public WebElement lastNameSrc;
+
+    @FindBy(id="SearchEmail")
+    public WebElement emailsearch;
+
+    @FindBy(id="search-customers")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//i[@class='fas fa-pencil-alt']")
+    public WebElement editButton;
+
+    @FindBy(xpath= "//td[text()='Registered']")
+    public WebElement validationsearch;
+
+    @FindBy(id="search-customers")
+    public WebElement searchButtonFirst;
+
+    @FindBy(id = "customer-delete")
+    public WebElement delete;
+
+    @FindBy(xpath="//button[@class='btn btn-danger float-right']")
+    public WebElement deleteAlert;
+
+    @FindBy(css =" input[placeholder='Search']")
+    public WebElement searchBox;
+
+
+
 
 
 
--- src/Proje_05/Proje_5_POM.java
+++ src/Proje_05/Proje_5_POM.java
@@ -1,25 +1,120 @@
 package Proje_05;
 
-import Utlity.BaseDriver;
 import Utlity.BaseDriverParameter;
+import Utlity.Tools;
+import javafx.scene.layout.Priority;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 
 
 public class Proje_5_POM extends BaseDriverParameter {
+
+
     @Test()
-    void loginTest(){
+    void loginTest() {
         CaseWebElements cwe = new CaseWebElements();
-        //Actions act = new Actions(BaseDriver.driver);
-     // act.moveToElement(cwe.loginButton).click().build().perform();
         cwe.email.clear();
         cwe.email.sendKeys("admin@yourstore.com");
         cwe.password.clear();
         cwe.password.sendKeys("admin");
         cwe.loginButton.click();
         Assert.assertTrue(cwe.Validation.getText().contains("Dashboard"));
-
     }
-}
+
+
+   @Test(priority = 1)
+
+   void navMenuCheck() {
+       CaseWebElements cwe = new CaseWebElements();
+
+       for (int i = 0; i < cwe.leftNawMenu.size(); i++) {
+
+           wait.until(ExpectedConditions.elementToBeClickable(cwe.leftNawMenu.get(i)));
+
+           cwe.leftNawMenu.get(i).click();
+
+           if (cwe.leftNawMenu.get(i).getAttribute("style").contains("display: block;")) ;
+           System.out.println("open");
+
+
+       }
+       for (int i = 0; i < cwe.leftNawMenu.size(); i++) {
+
+           boolean degisken = false;
+           if (cwe.leftNawMenu.size() > 0)
+               degisken = true;
+       }
+       System.out.println(" all elements visible");
+
+       }
+
+       @Test(priority = 6)
+    void addCustomer(){
+           CaseWebElements cwe = new CaseWebElements();
+           cwe.customersOne.click();
+           cwe.customersTwo.click();
+           cwe.addNew.click();
+           cwe.email.sendKeys("2study3@gmail.com");
+           cwe.password.sendKeys("123123");
+           cwe.firstName.sendKeys("yunus");
+           cwe.lastNamecustmr.sendKeys("demir");
+           cwe.dateOBirth.sendKeys("03/07/1993");
+           cwe.save.click();
+           Assert.assertTrue(cwe.validationCustomer.isDisplayed());
+       }
+
+
+       @Test(priority = 3)
+    void editCustomer(){
+           CaseWebElements cwe = new CaseWebElements();
+           cwe.customersOne.click();
+           cwe.customersTwo.click();
+            cwe.searchButtonFirst.click();
+            Tools.Bekle(1);
+            cwe.firstNamesearch.sendKeys("yunus");
+            cwe.lastNameSrc.sendKeys("demir");
+            cwe.emailsearch.sendKeys("2study3@gmail.com");
+            cwe.searchButton.click();
+            Assert.assertTrue(cwe.validationsearch.isDisplayed(),"TEST FAILED");
+            cwe.editButton.click();
+            cwe.firstName.clear();
+            cwe.firstName.sendKeys("enes");
+            cwe.save.click();
+            Assert.assertTrue(cwe.validationsearch.isDisplayed(),"TEST FAILED");
+
+       }
+
+       @Test(priority = 4)
+    void deleteCustomer(){
+           CaseWebElements cwe = new CaseWebElements();
+           cwe.customersOne.click();
+           cwe.customersTwo.click();
+           cwe.searchButtonFirst.click();
+           cwe.firstNamesearch.sendKeys("enes");
+           cwe.lastNameSrc.sendKeys("demir");
+           cwe.emailsearch.sendKeys("study3@gmail.com");
+           cwe.searchButton.click();
+           cwe.editButton.click();
+           cwe.delete.click();
+           cwe.deleteAlert.click();
+           Assert.assertTrue(cwe.validationsearch.isDisplayed(),"TEST FAILED");
+
+       }
+
+       @Test(priority = 5)
+    void searchFunction(){
+           CaseWebElements cwe = new CaseWebElements();
+           new Actions(driver).moveToElement(cwe.searchBox).click().sendKeys("Shipments").
+                   sendKeys(Keys.DOWN).sendKeys(Keys.ENTER).build().perform();
+           Tools.Bekle(3);
+           Assert.assertEquals(driver.getCurrentUrl(),"https://admin-demo.nopcommerce.com/Admin/Order/ShipmentList");
+
+       }
+
+   }
+
